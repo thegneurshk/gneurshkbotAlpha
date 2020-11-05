ea4efe50d0481db7a95499d4e5dbb6bc1a70744f
Oscujic <52619003+Oscujic@users.noreply.github.com>
2020-11-05 13:00:52 -0700

    can you see if this works? I don't know javascript

--- commands/wiggle.js
+++ commands/wiggle.js
@@ -2,50 +2,13 @@ module.exports = {
     name: 'wiggle',
     description: "this is a wiggle command",
     execute(message, args){
-
         const blankStr = '';
-        message.channel.send(`\
-        \nwiggle wiggle \
-        \n${blankStr.padEnd(1)}wiggle wiggle \
-        \n${blankStr.padEnd(2)}wiggle wiggle \
-        \n${blankStr.padEnd(3)}wiggle wiggle \
-        \n${blankStr.padEnd(5)}wiggle wiggle \
-        \n${blankStr.padEnd(7)}wiggle wiggle \
-        \n${blankStr.padEnd(9)}wiggle wiggle \
-        \n${blankStr.padEnd(12)}wiggle wiggle \
-        \n${blankStr.padEnd(15)}wiggle wiggle \
-        \n${blankStr.padEnd(18)}wiggle wiggle \
-        \n${blankStr.padEnd(21)}wiggle wiggle \
-        \n${blankStr.padEnd(24)}wiggle wiggle \
-        \n${blankStr.padEnd(27)}wiggle wiggle \
-        \n${blankStr.padEnd(30)}wiggle wiggle \
-        \n${blankStr.padEnd(33)}wiggle wiggle \
-        \n${blankStr.padEnd(35)}wiggle wiggle \
-        \n${blankStr.padEnd(37)}wiggle wiggle \
-        \n${blankStr.padEnd(39)}wiggle wiggle \
-        \n${blankStr.padEnd(40)}wiggle wiggle \
-        \n${blankStr.padEnd(41)}wiggle wiggle \
-        \n${blankStr.padEnd(42)}wiggle wiggle \
-        \n${blankStr.padEnd(41)}wiggle wiggle \
-        \n${blankStr.padEnd(40)}wiggle wiggle \
-        \n${blankStr.padEnd(39)}wiggle wiggle \
-        \n${blankStr.padEnd(37)}wiggle wiggle \
-        \n${blankStr.padEnd(35)}wiggle wiggle \
-        \n${blankStr.padEnd(33)}wiggle wiggle \
-        \n${blankStr.padEnd(30)}wiggle wiggle \
-        \n${blankStr.padEnd(27)}wiggle wiggle \
-        \n${blankStr.padEnd(24)}wiggle wiggle \
-        \n${blankStr.padEnd(21)}wiggle wiggle \
-        \n${blankStr.padEnd(18)}wiggle wiggle \
-        \n${blankStr.padEnd(15)}wiggle wiggle \
-        \n${blankStr.padEnd(12)}wiggle wiggle \
-        \n${blankStr.padEnd(9)}wiggle wiggle \
-        \n${blankStr.padEnd(7)}wiggle wiggle \
-        \n${blankStr.padEnd(5)}wiggle wiggle \
-        \n${blankStr.padEnd(3)}wiggle wiggle \
-        \n${blankStr.padEnd(2)}wiggle wiggle \
-        \n${blankStr.padEnd(1)}wiggle wiggle \
-        \nwiggle wiggle`);
+        const freq = 1;
+        const amp = 41;
+        for (i = 0; i < 41; i++) {
+            message.channel.send(
+                `\n${blankStr.padEnd((1 - Math.cos(freq*i)) * amp)}text`
+            );
+        }
     }
-
 }
