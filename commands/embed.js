94538666a7a2b4103c633f0ec9faaaa2e521cd11
gneurshk <73930447+lordgneurshk@users.noreply.github.com>
2020-11-15 14:07:08 -0700

    Add files via upload

---
+++ commands/embed.js
@@ -0,0 +1,29 @@
+const { MessageEmbed } = require('discord.js')
+
+module.exports = {
+    name: 'embed',
+    description: "this is a test command",
+    execute(message, args){
+        const exampleEmbed = new MessageEmbed()
+	    .setColor('#0099ff')
+	    .setTitle('Some title')
+	    .setURL('https://discord.js.org/')
+	    .setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
+	    .setDescription('Some description here')
+	    .setThumbnail('https://i.imgur.com/wSTFkRM.png')
+        .addFields(
+	       	{ name: 'Regular field title', value: 'Some value here' },
+	       	{ name: '\u200B', value: '\u200B' },
+	       	{ name: 'Inline field title', value: 'Some value here', inline: true },
+	       	{ name: 'Inline field title', value: 'Some value here', inline: true },
+	    )
+	    .addField('Inline field title', 'Some value here', true)
+	    .setImage('https://i.imgur.com/wSTFkRM.png')
+	    .setTimestamp()
+	    .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+
+        message.channel.send(exampleEmbed);
+        
+        
+    }
+}
