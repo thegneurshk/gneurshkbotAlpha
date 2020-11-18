3744ea6b093e31b4b15e001974540b2e10afbdd7
gneurshk <73930447+lordgneurshk@users.noreply.github.com>
2020-11-17 20:48:14 -0700

    Update bubblewrap.js
    allowed for user input in what the bubblewrap says and how big it is

--- commands/bubblewrap.js
+++ commands/bubblewrap.js
@@ -2,20 +2,38 @@ module.exports = {
     name: 'bubblewrap',
     description: "have fun popping",
     execute(message, args){
-        message.channel.send(`\
-        \n||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||\
-        \n||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||\
-        \n||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||\
-        \n||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||\
-        \n||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||\
-        \n||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||\
-        \n||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||\
-        \n||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||\
-        \n||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||\
-        \n||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||\
-        \n||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||\
-        \n||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||||     ||\
-        \n `);
+        len = args[0];
+        height = args[1];
+        text = args[2];
+
+        output = "";
+        if(args[2]) {
+            for (i = 0; i < height; i++) {
+                line = "";
+                for (j = 0; j < len; j++) {
+                    line += `||${text}||`;
+                }
+                output += line + "\n";
+            }
+            if(output.length > 2000) {
+                message.channel.send('exceeds 2000 character limit');
+            }else{
+                message.channel.send(output);
+            }
+        }else{
+            for (i = 0; i < height; i++) {
+                line = "";
+                for (j = 0; j < len; j++) {
+                    line += `||     ||`;
+                }
+                output += line + "\n";
+            }
+            if(output.length > 2000) {
+                message.channel.send('exceeds 2000 character limit');
+            }else{
+                message.channel.send(output);
+            }
+        }
     }
 
 }
