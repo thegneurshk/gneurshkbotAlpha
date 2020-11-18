79ebc0931900e2856fec818e51e7437fca7968fa
gneurshk <73930447+lordgneurshk@users.noreply.github.com>
2020-11-18 15:48:40 -0700

    Update wiggle2.js
    added a statement to check that all required arguments are defined

--- commands/wiggle2.js
+++ commands/wiggle2.js
@@ -3,26 +3,29 @@ module.exports = {
     description: "wiggle but modifiable",
     execute(message, args){
 
-        text = args[0];
-        duration = args[1];
-        amp = args[2];
-        freq = args[3];
-
-        var len = text.length - 1;
-
-        output = "";
-        for (i = 0; i < duration; i++) {
-               line = "";
-               for (j = 0; j < (1-Math.cos(i*freq))*amp; j++) {
-                   line += " ";
-                }
-            line += text;
-            output += line + "\n";
-        }
-        if(output.length > 2000) {
-            message.send.channel('exceeds 2000 character limit');
+        if(!args[3]){
+            message.channel.send(`make sure you have a defined text, duration, amplitude, and frequency \
+            \nuse \`|help wiggle2\` for usage`)
         }else{
-            message.channel.send(output);
+            text = args[0];
+            duration = args[1];
+            amp = args[2];
+            freq = args[3];
+
+            output = "";
+            for (i = 0; i < duration; i++) {
+                line = "";
+                for (j = 0; j < (1-Math.cos(i*freq))*amp; j++) {
+                    line += " ";
+                }
+                line += text;
+                output += line + "\n";
+            }
+            if(output.length > 2000) {
+                message.channel.send('exceeds 2000 character limit');
+            }else{
+                message.channel.send(output);
+            }
         }
     }
 }
