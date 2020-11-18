6e8c13c901ba05de60518b049d09add731b2f20e
gneurshk <73930447+lordgneurshk@users.noreply.github.com>
2020-11-18 15:47:14 -0700

    Update minesweeper.js
    added a statement to make sure all required arguments are defined

--- commands/minesweeper.js
+++ commands/minesweeper.js
@@ -2,42 +2,46 @@ module.exports = {
     name: 'minesweeper',
     description: "play minesweeper",
     execute(message, args){
-        var input1 = parseInt(args[0]);
-        var input2 = parseInt(args[1]);
-        var x = new Array(input1);
-        var chance;
-        if(args[2] === 'easy'){
-            chance = 0.3;
-        }else if(args[2] === 'medium'){
-            chance = 0.35;
-        }else if(args[2] === 'hard'){
-            chance = 0.45;
-        }else{
-            message.channel.send('make sure you have defined a board size and difficulty');
-        }
+        if(!args[2]) {
+            message.channel.send(`make sure you have defined a board size and difficulty \
+            \n use \`|help minsweeper\` for usage`);
 
-        output = ``;
-        for(i = 0; i < x.length; i++){
-            x[i] = new Array(input2);
-            line = '';
-            for(j = 0; j < x[i].length; j++){
-                var rand = Math.random();
-                if(rand < chance){
-                    var b = true;
-                }else{
-                    var b = false;
-                }
-                if(b){
-                    x[i][j] = `|| 💥 ||`;
-                    //counter++;
-                }else{
-                    x[i][j] = `||\`   \`||`;
-                }
-                line += x[i][j];
+        }else{
+            if(args[2] === 'easy'){
+                chance = 0.3;
+            }else if(args[2] === 'medium'){
+                chance = 0.35;
+            }else if(args[2] === 'hard'){
+                chance = 0.45;
             }
-            output += line + `\n`;
+            var input1 = parseInt(args[0]);
+            var input2 = parseInt(args[1]);
+            var x = new Array(input1);
+            var chance;
+            output = ``;
+            for(i = 0; i < x.length; i++){
+                x[i] = new Array(input2);
+                line = '';
+                for(j = 0; j < x[i].length; j++){
+                    var rand = Math.random();
+                    if(rand < chance){
+                        var b = true;
+                    }else{
+                        var b = false;
+                    }
+                    if(b){
+                        x[i][j] = `|| 💥 ||`;
+                    }else{
+                        x[i][j] = `||\`   \`||`;
+                    }
+                    line += x[i][j];
+                }
+                output += line + `\n`;
+            }
+            message.channel.send(output);
         }
-        message.channel.send(output);
+        
+        
         
     }
 }
