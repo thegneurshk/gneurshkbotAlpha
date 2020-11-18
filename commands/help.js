4b1a7405fba51a42c4bf63ac4d32c20dc9f5d091
gneurshk <73930447+lordgneurshk@users.noreply.github.com>
2020-11-17 20:50:14 -0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -290,6 +290,9 @@ module.exports = {
         }else if(args[0] === 'minesweeper'){
             message.channel.send(`define a board size and difficulty (easy/medium/hard) \
             \nexample: \`|minesweeper 10 10 easy\``)
+        }else if(args[0] === 'bubblewrap'){
+            message.channel.send(`sends a piece of bubblewrap with specified size and text\
+            \nexample: \`|bubblewrap 10 10 pop\``)
         }else{
             message.channel.send(`no help available for that argument`)
         }
