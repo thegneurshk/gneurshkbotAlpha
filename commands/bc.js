fe531bd9b1ff5d2720dc8a35fb0b9ff740f5c235
gneurshk <73930447+lordgneurshk@users.noreply.github.com>
2020-12-01 15:40:29 -0700

    Update bc.js

--- commands/bc.js
+++ commands/bc.js
@@ -2,12 +2,13 @@ module.exports = {
     name: 'bc',
     description: "blumpersugget coughkerstam",
     execute(message, args){
-        const first = [`biblibong`, `boilderdang`, `Berphremstein`, `boilderdang`, `bumpersticker`, `bimpystiff`,
-        `boppybluster`, `bavofsterble`, `blumpersugget`, `bifflyborp`, `bufgersomp`];
+        const first = [`biblibong`, `boilderdang`, `berphremstein`, `boilderdang`, `bumpersticker`, `bimpystiff`,
+        `boppybluster`, `bavofsterble`, `blumpersugget`, `bifflyborp`, `bufgersomp`, `bandersnatch`];
         const second = [`custerdang`, `cockledongle`, `cucumberstore`, `crimplesnitch`, `comforter`, `complepot`,
-        `cuckorparfk`, `crunchygrop`, `coughkerstam`, `compersant`, `cupplypork`];
+        `cuckorparfk`, `crunchygrop`, `coughkerstam`, `compersant`, `cupplypork`, `cummerbund`];
         const randFirst = first[Math.floor(Math.random() * first.length)];
         const randSecond = second[Math.floor(Math.random() * second.length)];
         message.channel.send(`${randFirst} ${randSecond}`);
+        
     }
 }
