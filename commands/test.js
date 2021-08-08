50e9d1097f6ea085794c842be99ec8e35563d762
gneurshk <73930447+lordgneurshk@users.noreply.github.com>
2021-08-08 17:20:48 -0600

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -3,6 +3,11 @@ module.exports = {
     description: "this is a test command",
     execute(message, args){
         message.channel.send('test clear');
+        //random facts
+        //triscuit is short for electricity biscuit, they were the first biscuits to be made with electricity
+        //a chinese emperor once escaped an assassin by running in circles around a pillar
+        //two prime numbers with a difference of 6 are known as sexy primes
+        //when a spider dies, its legs curl up due to a loss of blood pressure
+        //
     }
-
 }
