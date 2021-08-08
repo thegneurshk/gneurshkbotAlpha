d415fab0cc7d0516bd256f808ff57b84b41aaac5
gneurshk <73930447+lordgneurshk@users.noreply.github.com>
2021-08-08 17:16:06 -0600

    Update bc.js

--- commands/bc.js
+++ commands/bc.js
@@ -2,10 +2,12 @@ module.exports = {
     name: 'bc',
     description: "blumpersugget coughkerstam",
     execute(message, args){
-        const first = [`biblibong`, `boilderdang`, `berphremstein`, `boilderdang`, `bumpersticker`, `bimpystiff`,
-        `boppybluster`, `bavofsterble`, `blumpersugget`, `bifflyborp`, `bufgersomp`, `bandersnatch`];
-        const second = [`custerdang`, `cockledongle`, `cucumberstore`, `crimplesnitch`, `comforter`, `complepot`,
-        `cuckorparfk`, `crunchygrop`, `coughkerstam`, `compersant`, `cupplypork`, `cummerbund`];
+        const first = [`biblibong`, `boilderdang`, `berphremstein`, `broilerfnop`, `bumpersticker`, `bimpystiff`,
+        `boppybluster`, `bavofsterble`, `blumpersugget`, `bifflyborp`, `bufgersomp`, `bandersnatch`, `bompersnicker`,
+        `barklefutter`];
+        const second = [`custerdang`, `cockledongle`, `cucumberstore`, `crimplesnitch`, `comforter`, `complepot`,`beethiccsnufger`
+        `cuckorparfk`, `crunchygrop`, `coughkerstam`, `compersant`, `cupplypork`, `cummerbund`,`cumperbucket`,`crumpledtorch`,
+        `cankleinsurance`];
         const randFirst = first[Math.floor(Math.random() * first.length)];
         const randSecond = second[Math.floor(Math.random() * second.length)];
         message.channel.send(`${randFirst} ${randSecond}`);
